ef846cc83a717318d7d10aa33b9a1a3233917336
Alexander Baranov <scr4t@yandex.ru>
2017-04-19 17:27:37 +0300

    Replace file in XRootD backend; Support for backend suffixes

--- hepbackends/xrootd.py
+++ hepbackends/xrootd.py
@@ -2,16 +2,17 @@ from sh import xrdfs, xrdcopy
 from .base import BackendBase
 
 
-class XRootdBackend(BackendBase):
-    def __init__(self, xrd_server):
+class XrootdBackend(BackendBase):
+    def __init__(self, xrd_server, backend_suffix=""):
         self.xrd_server = xrd_server
+        self.suffix = backend_suffix
 
     def copy_from_backend(self, src_path, dst_path):
         xrdcopy("-r", self.xrd_server + src_path, dst_path)
 
     def copy_to_backend(self, src_path, dst_path):
         xrdfs(self.xrd_server, "mkdir", "-p", dst_path)
-        xrdcopy("-r", src_path, self.xrd_server + dst_path)
+        xrdcopy("-f", "-r", src_path, self.xrd_server + dst_path + self.suffix)
 
     def list_uploaded(self, path):
         files = xrdfs(self.xrd_server, "ls", path).split("\n")
